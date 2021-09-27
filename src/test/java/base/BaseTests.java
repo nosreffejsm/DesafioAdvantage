78152cee6014899d219352031ecb0a8ab79e4cd2
Jeff <jeff_sm@icloud.com>
2021-09-27 00:50:47 -0300

    Arquivo BaseTests Configurado com o webdriver

---
+++ src/test/java/base/BaseTests.java
@@ -0,0 +1,36 @@
+package base;
+
+import java.util.concurrent.TimeUnit;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import pages.HomePage;
+
+public class BaseTests {
+
+	private static WebDriver driver;
+	protected HomePage homePage;
+	
+	@BeforeAll
+	public static void inicializar() {
+		 WebDriverManager.chromedriver().setup();
+		 driver = new ChromeDriver();
+	}
+	
+	@BeforeEach
+	public void carregarPaginaInicial() {
+		driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+		driver.get("http://advantageonlineshopping.com/");
+		homePage = new HomePage(driver);
+	}
+	
+	@AfterAll
+	public static void finalizar() {
+		driver.quit();
+	}
+}
